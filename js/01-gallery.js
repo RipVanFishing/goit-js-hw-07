ac59490e4b82489eb07af15da4d1ca6a1eef279c
Denis Serkhel <ripvanfish27@gmail.com>
2021-12-26 18:34:33 +0200

    сделал разметку

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+function createImgMarkup(galleryItems) {
+   return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    }).join("");
+
+    
+}
+
+console.log(createImgMarkup(galleryItems));
