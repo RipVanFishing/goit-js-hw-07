d0b1970875cc8d1fec573a22dc7c1919f9d58236
Denis Serkhel <ripvanfish27@gmail.com>
2021-12-29 21:05:56 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
 
 
-function createImgMarkup(galleryItems) {
+
+const mainGalleryRef = document.querySelector(".gallery");
+const markupGallery = createImgMarkup(galleryItems);
+
+mainGalleryRef.insertAdjacentHTML('afterbegin', markupGallery);
+
+
+ function createImgMarkup(galleryItems) {
    return galleryItems.map(({ preview, original, description }) => {
         return `<div class="gallery__item">
   <a class="gallery__link" href="${original}">
@@ -19,6 +25,20 @@ function createImgMarkup(galleryItems) {
     }).join("");
 
     
+ }
+
+mainGalleryRef.addEventListener("click", clickOnImageCard);
+
+function clickOnImageCard(event) {
+event.preventDefault();
+    if (!event.target.classList.contains("gallery__link")) {
+        
+        return;
+    }
+     
+    console.log(event.target);
 }
 
-console.log(createImgMarkup(galleryItems));
+   
+
+
