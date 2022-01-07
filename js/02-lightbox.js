f8f4a08330246e0a3de4d60927e0bced018ccf78
Denis Serkhel <ripvanfish27@gmail.com>
2022-01-07 15:43:24 +0200

    сделал 2ю

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const mainGalleryRef = document.querySelector(".gallery");
+const markupGallery = createImgMarkup(galleryItems);
+
+mainGalleryRef.insertAdjacentHTML('afterbegin', markupGallery);
+
+
+
+ function createImgMarkup(galleryItems) {
+   return galleryItems.map(({ preview, original, description }) => {
+        return `
+      
+ <li> <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      title="${description}"
+    />
+  </a></li>
+`;
+    })
+    .join("");
+}
+
+
+let gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", () => {
+  captionsData: "title";
+  captionDelay: 250;
+});
