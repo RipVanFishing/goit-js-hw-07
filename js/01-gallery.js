1b1ca024c17316c2c145e23f693c830cf169d110
Denis Serkhel <ripvanfish27@gmail.com>
2022-01-07 15:32:52 +0200

    первое задание done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-
-
-
 const mainGalleryRef = document.querySelector(".gallery");
 const markupGallery = createImgMarkup(galleryItems);
 
 mainGalleryRef.insertAdjacentHTML('afterbegin', markupGallery);
 
+ mainGalleryRef.addEventListener('click', onGalleryClick)
 
  function createImgMarkup(galleryItems) {
    return galleryItems.map(({ preview, original, description }) => {
@@ -26,21 +24,29 @@ mainGalleryRef.insertAdjacentHTML('afterbegin', markupGallery);
    
  }
 
-const imgRef = mainGalleryRef.addEventListener('click', onGalleryClick)
+
 
 function onGalleryClick(event) {
-  
+  window.addEventListener("keydown", closeEsc);
   event.preventDefault();
   if (!event.target.classList.contains("gallery__image")) {
     return;
   }
 
-  console.log(event.target.getAttribute("src"));
-  return;
-}
+  const modalSrc = event.target.dataset.source;
+  const instance = basicLightbox.create(`<img src ="${modalSrc}" width="800" heigth="600">`);
+  instance.show();
+
+  function closeEsc(event) {
+     if (event.code === "Escape") {
+      instance.close();
+     
+     }
+    
+  }
+  }  
+
    
 
-const instance = basicLightbox.create(toString(imgRef));
 
-instance.show()
 
